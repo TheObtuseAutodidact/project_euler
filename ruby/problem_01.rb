4c5b5aad224af754552aa58f1e6d4cec1e9c433e
Obtuse <theobtuseautodidact@gmail.com>
2016-09-19 17:20:29 -0700

    Add ruby solution for problem 01

---
+++ ruby/problem_01.rb
@@ -0,0 +1,10 @@
+def three_and_five(n)
+  Array(1...n).reduce(0) do |sum, x|
+    x % 3 == 0 || x % 5 == 0 ? sum += x : sum
+  end
+end
+
+puts three_and_five(4) #  expecting 3
+puts three_and_five(6) # expecting 8
+puts three_and_five(7) # expecting 14
+puts three_and_five(1000) # expecting 14
